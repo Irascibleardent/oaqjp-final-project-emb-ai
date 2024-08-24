a4d26095d5aa7a3f988bfd4129b6c0c2d8a7f30a
IrascibleArdent <Irascible_Ardent@proton.me>
2024-08-23 23:55:44 -0400

    commit3

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,7 +6,22 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    formatted_response = json.dumps(response, indent=2)
+    formatted_response = json.loads(response.text)
 
-
-    return formatted_response
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+    dominant_emotion = max(formatted_response['emotionPredictions'][0]['emotion'], key=formatted_response['emotionPredictions'][0]['emotion'].get)
+    
+    output = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
+    
+    return output
